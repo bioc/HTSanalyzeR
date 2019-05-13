d3843d8b2cab5ac971d2fc11ffc01bb49b360739
lshep <lori.shepherd@roswellpark.org>
2019-05-13 10:25:27 -0400

    User requested Deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -39,3 +39,12 @@ cat(
 ##  and 'KeggGeneSets' was resolved
 ##- default download link of the BioGRID database in function 'biogridDataDownload' 
 ##  was updated to version 3.1.89 (tested on Jun 11, 2012)\n
+
+
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.11")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
+
